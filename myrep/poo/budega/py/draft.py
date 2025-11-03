c59c8c26e369aa1c000d6100f05e99a128271811
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-11-03 19:54:03 +0000

    a vida...a vida é triste...

---
+++ myrep/poo/budega/py/draft.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+
+
+
+class Market:
+    def __init__(self, counterCount: int, counter: str):
+        self.counterCount = counterCount
+        self.counter = counter
+        
