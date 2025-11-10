e0cbfb125df9efb4f250062d7f9566a89b43ed2a
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-11-10 18:02:02 +0000

    pausa pq to quase desmaiando de fome real

---
+++ myrep/poo/lapiseira/py/draft.py
@@ -0,0 +1,20 @@
+class Pencil:
+    def __init__(self, thickness: int, barrel, tip ):
+        self.__thickness = int
+        self.__barrel = Lead
+        self.__tip = 0
+        
+
+class Lead:
+    def __init__(self, hardness: float, thickness: int, size: int):
+        self.__hardness = hardness
+        self.__thickness = thickness
+        self.__size = size
+
+    def getSize(self):
+
+    def getThickness(self):
+
+    def getHardness(self):
+
+
