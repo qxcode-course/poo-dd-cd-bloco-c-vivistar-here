5d2351d6dcca781a925d0fe91f04faee916518b5
Vitoria de Jesus <vitoriadejesusstar@alu.ufc.br>
2025-11-15 01:05:00 +0000

    alguns defs comendo brownie e estudando ao som de lofi

--- myrep/poo/lapiseira/py/draft.py
+++ myrep/poo/lapiseira/py/draft.py
@@ -1,5 +1,27 @@
 class Pencil:
-    def __init__(self, thickness: int, tip, barrel)
+    def __init__(self, thickness: int, barrel:str):
         self.thickness = thickness
-        self.tip = tip
+        self.tip = Lead | None =None
         self.barrel = barrel    
+
+    def insert(self, lead: Lead) -> bool:
+        if self.thickness != lead.getThickness():
+            print("fail: gafrite não compatível")
+            return False
+        
+        self.barrel.append(lead)
+        return True
+    
+    def pull(self) -> bool:
+        if self.tip is not None:
+            print("fail: remova o grafite primeiro")
+            return False
+        
+        if not self.barrel:
+            print("fail: não tem grafite no tambor")
+            return False
+        
+        lead = self.barrel.pop(0)
+        self.tip = lead
+        return True
+    
